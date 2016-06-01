69d084736f57fc06effd1c9ff2bf2ff517ee59d1
Daniel Báscon <bascon1991@hotmail.es>
2016-06-01 19:57:19 +0200

    api

---
+++ wsgi/mybottleapp.py
@@ -0,0 +1,11 @@
+from bottle import route, default_app,template
+
+@route('/')
+    return template('index.tpl')
+
+# This must be added in order to do correct path lookups for the views
+import os
+from bottle import TEMPLATE_PATH
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
+
+application=default_app()
