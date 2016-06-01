6a240ce4c7b36dade11f933626e969c36c06ce3b
Daniel Báscon <bascon1991@hotmail.es>
2016-06-01 19:22:20 +0200

    appi

---
+++ wsgi/mybottleappy.py
@@ -0,0 +1,21 @@
+from bottle import route, default_app,template
+
+@route('/name/<name>')
+def nameindex(name='Stranger'):
+    return '<strong>Hello, %s!</strong>' % name
+ 
+@route('/')
+def index():
+    return '<strong>Hello World!</strong>'
+
+@route('/hello/')
+@route('/hello/<name>')
+def hello(name='Mundo'):
+    return template('template_hello.tpl', nombre=name)
+
+# This must be added in order to do correct path lookups for the views
+import os
+from bottle import TEMPLATE_PATH
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
+
+application=default_app()
