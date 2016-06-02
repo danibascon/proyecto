cf36954261f99499ae1a3277f11146875c1ad0e3
root <root@debian>
2016-06-02 11:13:04 +0200

    cambio

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -1,6 +1,14 @@
 from bottle import Bottle,route,request,template
 @route('/home')
-def home():	
+def index():	
 	return template('index.tpl')
 
+@error(500)
+def error500(error):
+	return template('404.tpl')
+# This must be added in order to do correct path lookups for the views
+import os
+TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
+
+application=default_app()
 
