3cc84ad98b7294e8a29a5543ee9e2996913ef338
dani <dani@localhost.localdomain>
2017-04-19 11:11:28 +0200

    subir

---
+++ proyecto.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+from sys import argv
+import bottle
+from bottle import Bottle,route,run,request,template,static_file
+from lxml import etree
+import requests
+
+import search.list
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = "AIzaSyAgqUvqAEBis51AIJtEVNHHqj8SNMoTqu0"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def youtube_search(options):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=options.q,
+    part="id,snippet",
+    maxResults=options.max_results
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+      videos.append("%s (%s)" % (search_result["snippet"]["title"],
+                                 search_result["id"]["videoId"]))
+    elif search_result["id"]["kind"] == "youtube#channel":
+      channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                   search_result["id"]["channelId"]))
+    elif search_result["id"]["kind"] == "youtube#playlist":
+      playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                    search_result["id"]["playlistId"]))
+
+  print "Videos:\n", "\n".join(videos), "\n"
+  print "Channels:\n", "\n".join(channels), "\n"
+  print "Playlists:\n", "\n".join(playlists), "\n"
+
+
+
+
+
+@route('/static/<filepath:path>')
+def server_static(filepath):
+	return static_file(filepath, root='static')
+
+
+if __name__ == '__main__':
+	run(host='0.0.0.0',port=argv[1])
+
+
