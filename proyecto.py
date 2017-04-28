6b3b5c7de8dcb22edb2003c4a66bf7b706f221e9
dani <dani@localhost.localdomain>
2017-04-28 12:54:39 +0200

    subir

--- proyecto.py
+++ proyecto.py
@@ -1,9 +1,55 @@
 from sys import argv
 import bottle
-from bottle import Bottle,route,run,request,template,static_file,redirect,get,post
+from bottle import Bottle,route,run,request,template,static_file,redirect,get,post, default_app, response, get, post
 import os
 import json
 import requests
+from requests_oauthlib import OAuth1
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import TokenExpiredError
+from urlparse import parse_qs
+
+
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token="
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+
+
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+
+TOKENS = {}
+
+###oauth1
+
+
+
+def get_request_token():
+    oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+    )
+    r = requests.post(url=REQUEST_TOKEN_URL, auth=oauth)
+    credentials = parse_qs(r.content)
+    TOKENS["request_token"] = credentials.get('oauth_token')[0]
+    TOKENS["request_token_secret"] = credentials.get('oauth_token_secret')[0]
+    
+def get_access_token(TOKENS):
+  
+  oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["request_token"],
+                   resource_owner_secret=TOKENS["request_token_secret"],
+                   verifier=TOKENS["verifier"],)
+  
+  
+  r = requests.post(url=ACCESS_TOKEN_URL, auth=oauth)
+  credentials = parse_qs(r.content)
+  TOKENS["access_token"] = credentials.get('oauth_token')[0]
+  TOKENS["access_token_secret"] = credentials.get('oauth_token_secret')[0]
+
+
+
+
 
 @route('/', method="get")
 def intro():
@@ -72,23 +118,67 @@ def inicio():
 
 
 
+#----------------------------------------------
+#Parte para twitter
 
 
+@get('/twitter')
+def twitter():
+    get_request_token()
+    authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
+    response.set_cookie("request_token", TOKENS["request_token"],secret='some-secret-key')
+    response.set_cookie("request_token_secret", TOKENS["request_token_secret"],secret='some-secret-key')
+    redirect authorize_url
+
+@get('/callback')
+
+def get_verifier():
+  TOKENS["request_token"]=request.get_cookie("request_token", secret='some-secret-key')
+  TOKENS["request_token_secret"]=request.get_cookie("request_token_secret", secret='some-secret-key')
+  TOKENS["verifier"] = request.query.oauth_verifier
+  get_access_token(TOKENS)
+  response.set_cookie("access_token", TOKENS["access_token"],secret='some-secret-key')
+  response.set_cookie("access_token_secret", TOKENS["access_token_secret"],secret='some-secret-key')
+  redirect('/twittear')
 
 
+@get('/twittear')
+def twittear():
+    if request.get_cookie("access_token", secret='some-secret-key'):
+      TOKENS["access_token"]=request.get_cookie("access_token", secret='some-secret-key')
+      TOKENS["access_token_secret"]=request.get_cookie("access_token_secret", secret='some-secret-key')
+      return template('tweet')  
+    else:
+      redirect('/twitter')
 
 
+@post('/twittear')
+def tweet_submit():
+  texto = request.forms.get("tweet")
+  TOKENS["access_token"]=request.get_cookie("access_token", secret='some-secret-key')
+  TOKENS["access_token_secret"]=request.get_cookie("access_token_secret", secret='some-secret-key')
+  print CONSUMER_KEY
+  print CONSUMER_SECRET
+  print TOKENS["access_token"]
+  print TOKENS["access_token_secret"]
+  oauth = OAuth1(CONSUMER_KEY,
+                   client_secret=CONSUMER_SECRET,
+                   resource_owner_key=TOKENS["access_token"],
+                   resource_owner_secret=TOKENS["access_token_secret"])
+  url = 'https://api.twitter.com/1.1/statuses/update.json'
+  r = requests.post(url=url,
+                      data={"status":texto},
+                      auth=oauth)
+  if r.status_code == 200:
+    return "<p>Tweet properly sent</p>"
+  else:
+    return "<p>Unable to send tweet</p>"+r.content
 
-
-
-
-
-
-
-
-
-
-
+@get('/twitter_logout')
+def twitter_logout():
+  response.set_cookie("access_token", '',max_age=0)
+  response.set_cookie("access_token_secret", '',max_age=0)
+  redirect('/twitter')
 
 
 
