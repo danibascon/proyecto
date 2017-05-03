5398cc0752656ce13f403ea317f672738c26299c
dani <dani@localhost.localdomain>
2017-05-03 12:20:00 +0200

    cambio

--- proyecto.py
+++ proyecto.py
@@ -150,7 +150,7 @@ def twittear():
     if request.get_cookie("access_token", secret='some-secret-key'):
       TOKENS["access_token"]=request.get_cookie("access_token", secret='some-secret-key')
       TOKENS["access_token_secret"]=request.get_cookie("access_token_secret", secret='some-secret-key')
-      return template('tweet',text=url) 
+      return template('tweet.tpl',text=url) 
     else:
       redirect('/twitter')
 
