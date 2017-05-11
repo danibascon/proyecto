b97a461e6c799d79478d7170ad601700680e29ae
dani <dani@localhost.localdomain>
2017-05-11 12:09:54 +0200

    cambio

--- proyecto.py
+++ proyecto.py
@@ -123,12 +123,11 @@ def letra():
   clave=os.environ["clave"]
   response.set_cookie("artista", artista,secret='some-secret-key')
   payloaad={"apikey":clave, "q":artista}
+
   g=requests.get('http://api.musixmatch.com/ws/1.1/track.search?',params=payloaad)
   dire=''
-
-  if r.status_code==200:
-    c=json.loads(r.text)
-
+  if g.status_code==200:
+    c=json.loads(g.text)
   for x in c['message']['body']['track_list'][0]['track']['track_share_url']:
     dire=dire+x
 
